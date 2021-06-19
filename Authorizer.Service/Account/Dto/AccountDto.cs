375474a7502f569d9d2759bf138cbe489e6df01c
DevDiegoADias <diego.acioly@hotmail.com>
2021-06-19 19:15:58 -0300

    Add project files.

---
+++ Authorizer.Service/Account/Dto/AccountDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authorizer.Service.Account.Dto
+{
+    public class AccountDto
+    {
+        public bool ActiveCard { get; set; }
+        public Decimal Limit { get; set; }
+
+        public String Name { get; set; }
+
+    }
+}
